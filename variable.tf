a6581af24233c0d23c7cc9b115dfcb377925dba4
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2023-04-14 10:27:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,15 +1,19 @@
 variable "vnet-name"{
     type = string
+    default = "vnet1"
 }
 
 variable "vnet-address" {
     type = list
+    default = ["10.0.0.0/16"]
 }
 
 variable "rg-name" {
     type = string
+    default = "vnet-rg"
 }
 
 variable "location" {
     type = string
+    default = "EastUS"
 }
